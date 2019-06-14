d7f373d124df776ccf6ba193c9b5e2ebd2e83e12
timothyhalley <timothy.halley@gmail.com>
2019-06-14 13:51:28 -0700

    DOTA2 refactoring

---
+++ src/mock/index.js
@@ -0,0 +1,19 @@
+import Mock from 'mockjs2'
+
+// Load the mock service when the environment is not prod or preview is true
+if (process.env.NODE_ENV !== 'production' || process.env.VUE_APP_PREVIEW === 'true') {
+  // Use synchronous load dependency
+  // Prevents GetInfo in vuex from running before mock, causing mock requests to return results
+  console.log('mock mounting')
+  require('./services/auth')
+  require('./services/user')
+  require('./services/manage')
+  require('./services/other')
+  require('./services/tagCloud')
+  require('./services/article')
+
+  Mock.setup({
+    timeout: 800 // setter delay time
+  })
+  console.log('mock mounted')
+}
