81fe91ab43dd67e3382e17a4c8b33c3b28948010
timothyhalley <timothy.halley@gmail.com>
2019-07-09 08:07:56 -0700

    Footer Update

--- src/components/GlobalFooter/GlobalFooter.vue
+++ src/components/GlobalFooter/GlobalFooter.vue
@@ -2,21 +2,21 @@
   <div class="footer">
     <div class="links">
       <a
-        href="https://pro.loacg.com/"
+        href="https://www.cascadezen.com/"
         target="_blank"
-      >Pro 首页</a>
+      >CascadeZen Home</a>
       <a
-        href="https://github.com/sendya/ant-design-pro-vue"
+        href="https://github.com/timothyhalley"
         target="_blank"
       >
         <a-icon type="github" />
       </a>
-      <a href="https://ant.design/">Ant Design</a>
-      <a href="https://vue.ant.design/">Vue Antd</a>
+      <a href="https://github.com/timothyhalley/dota2/">Code Repo</a>
+      <a href="https://github.com/timothyhalley/">Other Projects</a>
     </div>
     <div class="copyright">
       Copyright
-      <a-icon type="copyright" /> 2018 <span>白鹭学园技术组出品</span>
+      <a-icon type="copyright" /> 2020 <span>Product sourced by SkiCycleRun</span>
     </div>
   </div>
 </template>
